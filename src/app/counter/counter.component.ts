a8f4b5497f4c1a220dca1864d93b3ae1d5ba4a03
MiguelCarlosRojas <miguel.carlos@vallegrande.edu.pe>
2023-08-30 16:16:37 -0500

    Evita decremento en cero; bloquea botón.

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -6,19 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./counter.component.css']
 })
 export class CounterComponent {
-  counter = 0 ;
+  // Valor actual del contador
+  counter = 0;
+
+  // Controla si la disminución está habilitada
   activeDecrease = false;
-  increase(){
-    //
+
+  // Incrementa el contador en 1
+  increase() {
     this.counter++;
+
+    // Habilitar el botón de disminución cuando el contador es mayor que cero
+    this.activeDecrease = this.counter > 0;
   }
 
-  decrease(){
-    //
-    this.counter--;
+  // Decrementa el contador en 1 si es mayor que cero
+  decrease() {
+    if (this.counter > 0) {
+      this.counter--;
+    }
+
+    // Habilitar/deshabilitar el botón de disminución según el valor del contador
+    this.activeDecrease = this.counter > 0;
   }
 
-  inactiveDecrease(){
+  // Activa o desactiva la funcionalidad de disminución
+  inactiveDecrease() {
     this.activeDecrease = !this.activeDecrease;
   }
 }
